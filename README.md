6e544f7f39a3fa2b0a217ba8a9930b5830f82e32
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-03-26 21:49:01 -0300

    Add fuction to convert OID string to array

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Reportar
-Remote print reports
+Get SNMP information
+
+## Targets
+ - Get SNMP information
+ - Learn about Rust
+
+## Building
+`cargo build --release`
+
+## Doc
+`cargo doc -p reportar`
 
 ## TODO 
 - Add argument
-- Read json file
+- Read json config file
+- Write json output file.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use std::time::Duration;
 use snmp::{SyncSession, Value};
+use std::time::Duration;
+
+
+// This function convert a oid string to array.
+fn oid2array(oid: &str) -> Vec<u32> {
+    let mut x = Vec::new();
+
+    for item in oid.split('.') {
+        let i: u32 = item.parse::<u32>().unwrap();
+        x.push(i);
+    }
+
+    return x;
+}
 
 fn main() {
-    let sys_descr_oid = &[1,3,6,1,2,1,47,1,1,1,1,2,65536];
-    let agent_addr    = "10.135.174.1:161";
-    let community     = b"public";
-    let timeout       = Duration::from_secs(2);
-    
+    let oid = "1.3.6.1.2.1.47.1.1.1.1.2.65536";
+    let sys_descr_oid = &oid2array(oid);
+    let agent_addr = "10.135.174.1:161";
+    let community = b"public";
+    let timeout = Duration::from_secs(2);
+
     let mut sess = SyncSession::new(agent_addr, community, Some(timeout), 0).unwrap();
     let mut response = sess.get(sys_descr_oid).unwrap();
     if let Some((_oid, Value::OctetString(sys_descr))) = response.varbinds.next() {
@@ -15,4 +29,3 @@ fn main() {
         println!("Do data");
     }
 }
-
